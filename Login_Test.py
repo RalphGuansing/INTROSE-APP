1e31af6e2bd6717a93b972b567aafedbb29a6b17
Ralph Guansing <ralph_guansing@dlsu.edu.ph>
2017-12-04 22:54:27 +0800

    Made Login View
    +Temp Logo
    +simple Login View

---
+++ Login_Test.py
@@ -0,0 +1,86 @@
+import sys
+import pymysql
+from PyQt5 import QtWidgets,QtCore,Qt
+from LoginView import LoginView
+
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        #self.setFixedSize(1024,720)
+        self.resize(1024,720)
+        #self.resize(600,300)
+        
+        self.db = pymysql.connect("localhost","root","p@ssword","introse",autocommit=True)
+        self.cursor = self.db.cursor(pymysql.cursors.DictCursor)
+        
+        self.login_tab()
+        
+    def showMessage(self, title, message, info=None):
+        
+        """ This Method is responsible for Showing Dialogs if there is an error """
+        
+        infoBox = QtWidgets.QMessageBox()
+        infoBox.setIcon(QtWidgets.QMessageBox.Warning)
+        infoBox.setText(message)
+        if info is not None:
+            infoBox.setInformativeText(info)
+        infoBox.setWindowTitle(title)
+        #infoBox.setDetailedText("Detailed Text")
+        infoBox.setStandardButtons(QtWidgets.QMessageBox.Ok )
+        infoBox.setEscapeButton(QtWidgets.QMessageBox.Close) 
+        infoBox.exec_()
+    
+        
+    def login_tab(self):
+        self.setWindowTitle("LCG Veterinary Trading")
+        self.widgetFrame = WindowFrame(LoginView)
+        self.setCentralWidget(self.widgetFrame)    
+    
+#    def close_subFrame(self):
+#        
+#        """ Closes the Subwidget frame if it is visible"""
+#        
+#        try:
+#            if self.subFrame is not None:
+#                if self.subFrame.isVisible():
+#                    self.subFrame.close()
+#                    print("Closing Sub-Widget Frame")
+#                else:
+#                    print("Closing (with memory)")
+#        except:
+#            print("Closing")
+#    
+#    def closeEvent(self, event):
+#        
+#        """ Adding more functions when the mainwindow closes """
+#        
+#        self.db.close()
+#        self.close_subFrame()
+#        self.close()
+   
+            
+        
+
+class WindowFrame(QtWidgets.QWidget):
+    
+    def __init__(self, layout, extra=None):
+        super().__init__()
+        
+        if extra is None:
+            self.layout = layout(self)
+        else:
+            self.layout = layout(self, extra)
+        
+        self.setLayout(self.layout)
+        
+        
+if __name__ == "__main__":    
+    app = QtWidgets.QApplication(sys.argv)
+    
+    a_window = MainWindow()
+    a_window.show()
+    sys.exit(app.exec_())
+    
