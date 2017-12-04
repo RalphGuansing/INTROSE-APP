0a0ce410c90d5c42c0384645b1031dbebe2f0c00
Densolator <raffaeld12@gmail.com>
2017-12-05 00:40:21 +0800

    Invoice GUI
    Upload of Invoice GUI

---
+++ HomeInvoice.py
@@ -0,0 +1,41 @@
+import sys
+from PyQt5 import QtWidgets,QtCore
+
+
+class HomeInvoice(QtWidgets.QGridLayout):
+    def __init__(self, frame):
+        super().__init__()
+        self.frame = frame
+        self.init_ui()
+
+    def init_ui(self):
+
+        self.bAddInvoice = QtWidgets.QPushButton("Add Invoice")
+        self.bAddInvoice.setStyleSheet('QPushButton { font-size: 12pt; padding: 10px;}')
+        self.bAddInvoice.setFixedWidth(200)
+        
+        self.bViewInvoice = QtWidgets.QPushButton("View Invoice")
+        self.bViewInvoice.setStyleSheet('QPushButton { font-size: 12pt; padding: 10px;}')
+        self.bViewInvoice.setFixedWidth(200)
+  
+        
+        self.lInvoiceNum = QtWidgets.QLabel("Enter Invoice Number:")
+        self.lInvoiceNum.setStyleSheet('QLabel { font-size: 12pt; padding: 10px;}')	  
+
+        self.bInvoiceList = QtWidgets.QPushButton("View Invoice List")
+        self.bInvoiceList.setStyleSheet('QPushButton { font-size: 12pt; padding: 10px;}')
+        self.bInvoiceList.setFixedWidth(200)      
+        
+        self.tInvoiceNum = QtWidgets.QLineEdit(self.frame)
+        self.tInvoiceNum.setFixedWidth(50)
+        
+        self.setColumnStretch(4,1)
+        self.setColumnStretch(1,1)
+        self.setRowStretch(16,1)
+
+        #Add Widgets
+        self.addWidget(self.bAddInvoice, 1, 2, 1, 1)   
+        self.addWidget(self.lInvoiceNum, 2, 2, 1, 1)
+        self.addWidget(self.tInvoiceNum, 2, 3, 1, 1)        
+        self.addWidget(self.bViewInvoice, 3, 2, 1, 1)     
+        self.addWidget(self.bInvoiceList, 4, 2, 1, 1)          
