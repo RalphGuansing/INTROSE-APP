ffd49cc12164954a39f83b0d49690787b7cb1083
Ralph Guansing <ralph_guansing@dlsu.edu.ph>
2017-12-05 18:08:00 +0800

    Login and Home
    Added:
     + Login
     + Home
    
    Incomplete:
     - show more info if inputs are invalid
     - Navbar functionality

--- Login_Test.py
+++ Login_Test.py
@@ -3,8 +3,7 @@ import pymysql
 import hashlib
 from PyQt5 import QtWidgets,QtCore,Qt
 from LoginView import LoginView
-
-
+from HomeView import HomeView
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -13,8 +12,7 @@ class MainWindow(QtWidgets.QMainWindow):
         super(MainWindow, self).__init__(parent)
         #self.setFixedSize(1024,720)
         self.resize(1024,720)
-        #self.resize(600,300)
-        
+
         self.db = pymysql.connect("localhost","root","p@ssword","introse",autocommit=True)
         self.cursor = self.db.cursor(pymysql.cursors.DictCursor)
         
@@ -35,45 +33,48 @@ class MainWindow(QtWidgets.QMainWindow):
         infoBox.setEscapeButton(QtWidgets.QMessageBox.Close) 
         infoBox.exec_()
     
-        
     def login_tab(self):
         self.setWindowTitle("LCG Veterinary Trading")
-        self.widgetFrame = WindowFrame(LoginView)
+        self.widgetFrame = SubWindowFrame(LoginView)
         self.setCentralWidget(self.widgetFrame)    
-        self.widgetFrame.layout.bLogin.clicked.connect(self.hash_password)  
+        self.widgetFrame.layout.bLogin.clicked.connect(self.login)
     
-    #to be edited later
-    def hash_password(self):
-        plaintext = self.widgetFrame.layout.tPassword.text().encode('utf-8')
-        print(plaintext)
-        sha = hashlib.sha1(plaintext)
-        print(sha.hexdigest())
-#    def close_subFrame(self):
-#        
-#        """ Closes the Subwidget frame if it is visible"""
-#        
-#        try:
-#            if self.subFrame is not None:
-#                if self.subFrame.isVisible():
-#                    self.subFrame.close()
-#                    print("Closing Sub-Widget Frame")
-#                else:
-#                    print("Closing (with memory)")
-#        except:
-#            print("Closing")
-#    
-#    def closeEvent(self, event):
-#        
-#        """ Adding more functions when the mainwindow closes """
-#        
-#        self.db.close()
-#        self.close_subFrame()
-#        self.close()
-   
-            
+    def home_tab(self):
+        self.setWindowTitle("Home")
+        self.widgetFrame = WindowFrame(HomeView)
+        self.setCentralWidget(self.widgetFrame)    
+        #self.widgetFrame.layout.bLogin.clicked.connect(self.login)
         
+    def login(self):
+        username = self.widgetFrame.layout.tUsername.text()
+        
+        encoded_plaintext = self.widgetFrame.layout.tPassword.text().encode('utf-8')
+        sha = hashlib.sha1(encoded_plaintext)
+        password = sha.hexdigest()
+        
+        select_statement = """select employee_id, username, concat(first_name, ' ' , last_name) as full_name
+            from employee
+            where username = '"""+ username +"""' and password = '"""+ password +"""'"""
+        
+        print(select_statement)
+        self.cursor.execute(select_statement)
+        
+        tempvar = self.cursor.fetchone()
+        
+        #CORRECT
+        if tempvar is not None:
+            print("Employee ID: " + str(tempvar["employee_id"]))
+            print("Username: " + str(tempvar["username"]))
+            print("Full Name: " + str(tempvar["full_name"]))
+            
+            self.username = tempvar["username"]
+            self.home_tab()
+            
+        #WRONG OR DOES NOT EXIST
+        else:
+            self.showMessage("Invalid Entry", "Invalid username or password")
 
-class WindowFrame(QtWidgets.QWidget):
+class SubWindowFrame(QtWidgets.QWidget):
     
     def __init__(self, layout, extra=None):
         super().__init__()
@@ -85,9 +86,72 @@ class WindowFrame(QtWidgets.QWidget):
         
         self.setLayout(self.layout)
         
+
+class WindowFrame(QtWidgets.QWidget):
+    
+    def __init__(self, layout, extra=None):
+        super().__init__()
+        
+        self.container_layout = QtWidgets.QVBoxLayout()
+        self.setContentsMargins(0,0,0,0)
+        
+        #ADDING NAVBAR
+        self.make_navbar()
+        self.container_layout.addWidget(self.navbar)
+        
+        if extra is None:
+            self.layout = layout(self)
+        else:
+            self.layout = layout(self, extra)
+        
+        self.layout_groupbox = QtWidgets.QGroupBox("")
+        self.layout_groupbox.setLayout(self.layout)
+        self.container_layout.addWidget(self.layout_groupbox)
+
+        self.setLayout(self.container_layout)
+        
+    def make_navbar(self):
+        self.navbar = QtWidgets.QGroupBox("")
+        #self.navbar.setFlat(True)
+        self.navbar.setStyleSheet("QGroupBox{border:0; background-color:#ca4f4f;}")
+        
+        navGrid = QtWidgets.QGridLayout()
+        
+        labelStyle = """QLabel { font-size: 12pt; color: white; padding: 4px; font-family:Montserrat;}
+                        QLabel:hover{color:gainsboro;}
+                     """
+        labelStyle2 = """QLabel { font-size: 24pt; color: white; padding: 1px; font-family:Montserrat;}
+                         QLabel:hover{color:gainsboro;}
+                      """
+        
+        self.bLogo = QtWidgets.QLabel("LCG")
+        self.bLogo.setStyleSheet(labelStyle2)
+        
+        self.bInvoice = QtWidgets.QLabel("Invoice")
+        self.bInvoice.setStyleSheet(labelStyle)
+        
+        self.bInventory = QtWidgets.QLabel("Inventory")
+        self.bInventory.setStyleSheet(labelStyle)
+        
+        self.bAccounting = QtWidgets.QLabel("Accounting")
+        self.bAccounting.setStyleSheet(labelStyle)
+        
+        self.bLogout = QtWidgets.QLabel("Log Out")
+        self.bLogout.setStyleSheet(labelStyle)
+        
+        navGrid.setColumnStretch(5,1)
+        navGrid.addWidget(self.bLogo, 1,1)
+        navGrid.addWidget(self.bInvoice, 1,2)
+        navGrid.addWidget(self.bInventory,1,3)
+        navGrid.addWidget(self.bAccounting,1,4)
+        navGrid.addWidget(self.bLogout,1,6)
+        
+        self.navbar.setLayout(navGrid)
+
         
 if __name__ == "__main__":    
     app = QtWidgets.QApplication(sys.argv)
+    Qt.QFontDatabase.addApplicationFont("Resources/Montserrat-Regular.ttf")
     
     a_window = MainWindow()
     a_window.show()
