56e717c0d754f7f67f77d3b01454bcbfa145e7cf
Densolator <raffaeld12@gmail.com>
2017-12-11 11:53:14 +0800

    OY FRITz

---
+++ GUI/Error/error.py
@@ -0,0 +1,11 @@
+import sys
+from PyQt5 import QtWidgets,QtCore
+
+class errorMessage(QtWidgets.QtWidget):
+	def __init__(self, frame=None):
+		self.QInputDialog()
+
+if __name__ == '__main__':
+	temp = errorMessage()
+
+	
